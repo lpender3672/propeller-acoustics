nice sweep graph still have an incorrect order of magnitude will plot FM against solidity

app/betz.py
@@ -194,7 +194,7 @@ def betz_off_design(av):
     iters = 0
 
     while (iters < 100):
-        if (np.max(dphi / phi) < 1e-3):
+        if (np.max(np.abs(dphi / phi)) < 1e-3):
             break
 
         # Analysis of Arbitrary Designs
@@ -262,21 +262,20 @@ def betz_off_design(av):
     #CT_prime = (np.pi ** 3 / 4) * sigma * Cz * xi * F**(3/2) / ((F + sigma * K_prime) * np.cos(phi))**2
     #CP_prime = CT_prime * np.pi * xi * Cx / Cz * np.cos(sweep)
     Wsq = (V * (1 + a)) ** 2 + (Ucorr * (1 - a_prime)) ** 2
-    CT_prime = 1 / 2 * Wsq * c * (Cl * np.cos(phi) - Cd * np.sin(phi))
-    CP_prime = 1 / 2 * Wsq * c * (Cl * np.sin(phi) + Cd * np.cos(phi)) * R * xi * np.cos(sweep)
+    T_prime = 1 / 2 * B * Wsq * c * (Cl * np.cos(phi) - Cd * np.sin(phi))
+    Q_prime = 1 / 2 * B * Wsq * c * (Cl * np.sin(phi) + Cd * np.cos(phi)) * xi * R * np.cos(sweep)
+    P_prime = Omega * Q_prime
 
     A = np.pi * R**2
-    CT_prime /= A * (R * Omega) ** 2
-    CP_prime /= A * R ** 3 * Omega ** 2
-
-
-
+    CT_prime = T_prime / (1/2 * A * (R * Omega) ** 2)
+    CP_prime = P_prime / (1/2 * A * (R * Omega) ** 3)
+    
     print(Re_c)
 
-    CT = np.trapz(CT_prime, xi)
-    CP = np.trapz(CP_prime, xi)
+    CT = np.trapz(CT_prime, xi * R)
+    CP = np.trapz(CP_prime, xi * R)
 
-    FM = np.abs(CT) ** (2/3) / np.abs(CP)
+    FM = CT ** (2/3) / (np.sqrt(2) * np.abs(CP))
 
     av.res['CT'] = CT
     av.res['CP'] = CP

app/geometry.py
@@ -3,18 +3,18 @@ import numpy as np
 
 from scipy.integrate import cumtrapz
 
-def rotate(X,Y,theta):
-    # function to rotate coordinates in the X-Y plane
-    return (X[:]*np.cos(theta) - Y[:]*np.sin(theta)), (X[:]*np.sin(theta) + Y[:]*np.cos(theta)) 
+def rotate(X,Z,theta):
+    # function to rotate coordinates in the X-Z plane
+    return (X[:]*np.cos(theta) - Z[:]*np.sin(theta)), (X[:]*np.sin(theta) + Z[:]*np.cos(theta)) 
 
 
 def generate_blade_mesh(av):
     # adapted from code by Lily Board phd
 
     chord, twist, radius = av.prop['c'], av.prop['twist'], av.prop['r0']
-    xnf, ynf = av.airfoil_data[:,0], av.airfoil_data[:,1]
+    xnf, znf = av.airfoil_data[:,0], av.airfoil_data[:,1]
     xf = np.interp(np.linspace(0, 1, av.prop['nx']), np.linspace(0, 1, xnf.shape[0]), xnf)
-    yf = np.interp(np.linspace(0, 1, av.prop['nx']), np.linspace(0, 1, ynf.shape[0]), ynf)
+    zf = np.interp(np.linspace(0, 1, av.prop['nx']), np.linspace(0, 1, znf.shape[0]), znf)
 
     xf -= 0.5 # quarter chord
 
@@ -25,30 +25,30 @@ def generate_blade_mesh(av):
     nf = xf.shape[0]
     Nsect = radius.shape[0]
     X = np.zeros((Nsect+2,nf))
-    Y = np.zeros((Nsect+2,nf))
     Z = np.zeros((Nsect+2,nf))
+    Y= np.zeros((Nsect+2,nf))
 
     # at 0 only rotate about radial
-    sx, Y[0,:] = rotate(xf*chord[0], yf*chord[0], -twist[0])
+    sx, Z[0,:] = rotate(xf*chord[0], zf*chord[0], -twist[0])
     thetas = sx / radius[0] + sweep_angle[0]
     thetas = np.clip(thetas, -np.pi/2, np.pi/2)
     X[0,:] = radius[0] * np.sin(thetas)
-    Z[0,:] = 0.0
+    Y[0,:] = 0.0
     for i in range(1,Nsect+1):
         # first rotate about radial direction
-        sx, sy = rotate(xf*chord[i-1], yf*chord[i-1], -twist[i-1])
+        sx, sz = rotate(xf*chord[i-1], zf*chord[i-1], -twist[i-1])
         # warp chordwise foil to at current radius
         thetas = sx / radius[i-1] + sweep_angle[i-1] # appy sweep
         thetas = np.clip(thetas, -np.pi/2, np.pi/2)
-        Y[i,:] = sy
+        Z[i,:] = sz
         X[i,:] = radius[i-1] * np.sin(thetas)
-        Z[i,:] = radius[i-1] * np.cos(thetas)
-    sx, sy = rotate(xf*chord[Nsect-1], yf*chord[Nsect-1], -twist[Nsect-1])
+        Y[i,:] = radius[i-1] * np.cos(thetas)
+    sx, sy = rotate(xf*chord[Nsect-1], zf*chord[Nsect-1], -twist[Nsect-1])
     thetas = sx / radius[Nsect-1] + sweep_angle[Nsect-1]
     thetas = np.clip(thetas, -np.pi/2, np.pi/2)
-    Y[Nsect+1,:] = sy
+    Z[Nsect+1,:] = sy
     X[Nsect+1,:] = radius[Nsect-1] * np.sin(thetas)
-    Z[Nsect+1,:] = Z[Nsect,:]
+    Y[Nsect+1,:] = Y[Nsect,:]
     
     coords = np.zeros((nf*(Nsect+2),3))
     # loop over blade elements

app/hanson_comp.py
@@ -4,8 +4,46 @@ from scipy.integrate import trapz, simps, cumtrapz
 
 from matplotlib import pyplot as plt
 from matplotlib import cm
-from betz import bem
+from betz import bem, betz_off_design
 from hanson import hanson, calc_noise_components
+
+from xfoil import XFoil
+from xfoil.model import Airfoil
+
+def foil_data(airfoil_data, alpha, Re):
+    
+    xf = XFoil()
+    xf.airfoil = Airfoil(
+        airfoil_data[:,0],
+        airfoil_data[:,1]
+        )
+    xf.Re = Re
+    xf.M = 0.0
+    xf.max_iter = 100
+    xf.verbose = False
+
+    if isinstance(alpha, float):
+        cls = np.zeros(1)
+        cds = np.zeros(1)
+        alpha = [alpha]
+    else:
+        cls = np.zeros(len(alpha))
+        cds = np.zeros(len(alpha))
+
+    for i, a in enumerate(alpha):
+        out = xf.a(a)
+        print(out)
+        cl, cd, _, _ = out
+        cls[i] = cl
+        cds[i] = cd
+
+    return cls, cds
+
+def run_xfoil(airfoil_data):
+
+    alphas = np.linspace(-20, 20, airfoil_data.shape[0])
+    cls, cds = foil_data(airfoil_data, alphas, 1e6)
+    return np.column_stack((airfoil_data, alphas, cls, cds))
 
 class AppVars():
     def __init__(self):
@@ -52,7 +90,7 @@ def radial_bessel(oper: dict, prop: dict):
     ax.legend()
     plt.show()
 
-def radial_locus(oper: dict, prop: dict):
+def radial_locus(oper: dict, prop: dict, ax=None, colour=None, label=None):
 
     # calc dopfac for observer
     oper['Min'] = 0;                  #% inflow Mach number [-]
@@ -70,8 +108,6 @@ def radial_locus(oper: dict, prop: dict):
 
     theta = 3 * np.pi / 4 # np.arange(0, np.pi, 0.01)
 
-    fig, ax = plt.subplots()
-
     dopfac_o = 1 - oper['Mfl'] * np.cos(theta)
     r = 10 * prop['rt']
     y = r * np.sin(theta)
@@ -83,6 +119,8 @@ def radial_locus(oper: dict, prop: dict):
     PDm = np.zeros((Nrs), dtype=complex)
     PLm = np.zeros((Nrs), dtype=complex)
 
+    _, xc = np.meshgrid(prop['r0_rt'], prop['xc'])
+
     for i, m in enumerate(np.arange(1, Nms + 1)):
         fac = 2 * m * prop['B'] / (oper['Mr'] * dopfac_o)
         yofac = oper['Mr'] ** 2 * np.cos(theta) - oper['Mx']
@@ -93,7 +131,7 @@ def radial_locus(oper: dict, prop: dict):
         phis = fac * oper['Mt'] * prop['MCA'] / (2 * prop['rt'])
 
         # large term top of p5
-        term1 = - (oper['rho'] * oper['c0']**2 * prop['B'] * np.sin(theta) * np.exp(1j * m * prop['B'] * (omegaDop_o * r / oper['c0'] - np.pi / 2))) / ((8 * np.pi * y / (2 * prop['rt']) * dopfac_o) + 1e-10)
+        term1 = - (oper['rho'] * oper['c0']**2 * prop['B'] * np.sin(theta) * np.exp(1j * m * prop['B'] * (omegaDop_o * r / oper['c0'] - np.pi / 2))) / ((8 * np.pi * y / (2 * prop['rt']) * dopfac_o))
 
         bess = besselj(m * prop['B'], m * prop['B'] * prop['r0_rt'] * oper['Mt'] * np.sin(theta) / dopfac_o)
         
@@ -118,16 +156,17 @@ def radial_locus(oper: dict, prop: dict):
         PDm += I2plt
         PLm += I3plt
 
+    if ax is None:
+        fig,ax = plt.subplots(2, 2)
+    
+    ax[0,0].quiver(PVm.real[:-1], PVm.imag[:-1], np.diff(PVm.real), np.diff(PVm.imag), angles='xy', scale_units='xy', scale=1, label=label, color=colour)
+    ax[0,1].quiver(PLm.real[:-1], PLm.imag[:-1], np.diff(PLm.real), np.diff(PLm.imag), angles='xy', scale_units='xy', scale=1, color=colour)
+    ax[1,0].quiver(PDm.real[:-1], PDm.imag[:-1], np.diff(PDm.real), np.diff(PDm.imag), angles='xy', scale_units='xy', scale=1, color=colour)
+    total = PVm + PLm + PDm
+    ax[1,1].quiver(total.real[:-1], total.imag[:-1], np.diff(total.real), np.diff(total.imag), angles='xy', scale_units='xy', scale=1, color=colour)
 
-    viridis = cm.get_cmap('viridis')
-    clrs = viridis(prop['r0_rt'])
-
-    ax.plot(PVm.real, PVm.imag, label=f'Thickness, m={m}')
-    ax.plot(PLm.real, PLm.imag, label=f'Lift m={m}')
-    ax.plot(PDm.real, PDm.imag, label=f'Drag m={m}')
-
-    ax.grid()
-    ax.legend()
+    for axi in ax.flatten():
+        axi.grid(True)
 
 
 def chord_locus(oper: dict, prop: dict):
@@ -146,6 +185,8 @@ def chord_locus(oper: dict, prop: dict):
     dopfac_o = 1 - oper['Mfl'] * np.cos(theta)
     r = 10 * prop['rt']
     y = r * np.sin(theta)
+
+    _, xc = np.meshgrid(prop['r0_rt'], prop['xc'])
 
     for i, m in enumerate(np.arange(1, 2)):
         fac = 2 * m * prop['B'] / (oper['Mr'] * dopfac_o)
@@ -246,18 +287,87 @@ def hanson_sweep(oper: dict, prop: dict):
     ax.set_xlabel('Tip Sweep [rad]')
     ax.set_ylabel('SPL [dB]')
 
+def radial_locus_sweep(oper: dict, prop: dict):
 
-if __name__ == "__main__":
+    fig, ax = plt.subplots(2, 2, figsize=(6, 6))
+    ax[0,0].set_title('Thickness')
+    ax[0,1].set_title('Lift')
+    ax[1,0].set_title('Drag')
+    ax[1,1].set_title('Total')
+
+    # set equal aspect ratio
+    for axi in ax.flatten():
+        axi.set_aspect('equal', 'box')
+
+    max_sweep = np.pi/4
+    nsweeps = 8
+    sweep = np.linspace(0, max_sweep, nsweeps)
+    SPL = np.zeros((nsweeps, 3))
+    for i in range(nsweeps):
+        prop['sweep'] = np.linspace(0, sweep[i], prop['nr'])
+
+        colour = cm.jet(i/nsweeps)
+        radial_locus(oper, prop, ax=ax, colour=colour, label=f'$\psi= {sweep[i]*180/np.pi:.2f}$')
+
+    fig.legend(loc='lower center', bbox_to_anchor=(0.5, 0.0), ncol=4, frameon=False)
+    fig.tight_layout()
+    fig.subplots_adjust(bottom=0.15)  # Extra space for the legend
+
+def operating_range(av, ):
+
+    Js = np.linspace(-0.1, 0.2, 100)
+    CPs = np.zeros(Js.shape)
+    CTs = np.zeros(Js.shape)
+    FMs = np.zeros(Js.shape)
+
+    for i,J in enumerate(Js):
+        av.oper['V'] = J * av.oper['Omega'] * av.prop['rt']
+        av = betz_off_design(av)
+
+        if not av.res['converged']:
+            CPs[i] = np.nan
+            CTs[i] = np.nan
+            FMs[i] = np.nan
+            continue
+
+        ivlds = av.res['invalids']
+        if (len(ivlds) > 0 and ivlds[-1] > av.prop['nr'] // 2):
+            CPs[i] = np.nan
+            CTs[i] = np.nan
+            FMs[i] = np.nan
+            continue
+
+        CPs[i] = av.res['CP']
+        CTs[i] = av.res['CT']
+        FMs[i] = av.res['FM']
+
+    fig, ax = plt.subplots()
+    ax.plot(Js[CTs > 0], CPs[CTs > 0], 'o-', label='CP')
+    ax.plot(Js[CTs > 0], CTs[CTs > 0], 'o-', label='CT')
+
+    ax.legend()
+    ax.grid()
+
+    fig, ax = plt.subplots()
+    ax.plot(Js[CTs > 0],  (FMs)[CTs > 0], label='FM')
+
+    ax.legend()
+    ax.grid()
+
+    plt.show()
+
+def main():
     oper = {
-        'V':  50.0,
+        'V':  0.01,
         'c0': 343,
-        'Omega': 10000 * 2 * np.pi / 60,
+        'Omega': 20000 * 2 * np.pi / 60,
         'rho': 1.225,
-        'pref': 2e-5
+        'pref': 2e-5,
+        'nu': 1.48e-5
     }
     prop = {
-        'rt': 0.01,
-        'rh': 0.001,
+        'rt': 0.5,
+        'rh': 0.05,
         'B': 2,
         'nr' : 20,
         'nx' : 50
@@ -265,11 +375,12 @@ if __name__ == "__main__":
     prop['r0_rt'] = np.linspace(prop['rh'], prop['rt'], prop['nr']) / prop['rt']
     prop['c'] = 0.1 * prop['rt'] * np.ones(prop['nr'])
     prop['xc'] = np.linspace(-0.5, 0.5, prop['nx'])
-    prop['twist'] = 0.1 + 0.01 / prop['r0_rt']
+    prop['twist'] = 5 + 5 * np.arctan(1 / prop['r0_rt'])
+    prop['twist'] *= np.pi / 180
     #prop['sweep'] = np.zeros(prop['nr'])
-    prop['sweep'] = np.linspace(0, 1.2, prop['nr'])
+    prop['sweep'] = np.linspace(0, 0.01, prop['nr'])
 
-    airfoil_data = np.loadtxt('app/foils/naca0012.surf')
+    airfoil_data = np.loadtxt('app/foils/naca2412.surf')
     prop['Bd'] = prop['c'] / (2 * prop['rt'])
     n = airfoil_data.shape[0]
     xf = np.interp(np.linspace(0,1, 2*prop['nx']), np.linspace(0,1, n), airfoil_data[:, 0])
@@ -284,20 +395,34 @@ if __name__ == "__main__":
 
     prop['tb'] = np.max(tf) * prop['c']
     
-    prop['sweep'] = np.linspace(0, 1.5, prop['nr']) ** 2
-
     av = AppVars()
     av.oper = oper
     av.prop = prop
+
+    airfoil_data = run_xfoil(airfoil_data)
     av.airfoil_data = airfoil_data
 
-    bem(av)
-    res = av.res
-    alpha = res['alpha']
-    alpha = prop['twist'] - np.arctan( oper['V'] / (oper['Omega'] * prop['r0_rt'] * prop['rt']) )
+    av = betz_off_design(av)
 
-    prop['Cl_r'] = (2 * np.pi * alpha) #* np.cos(prop['sweep'])**2
-    prop['Cd_r'] = (0.0087 - 0.021 * alpha + 0.400 * alpha ** 2) #* np.cos(prop['sweep'])**2
+    if not av.res['converged']:
+        print("BEM failed")
+        return
+
+    res = av.res
+
+    alpha = av.res['alpha']
+    sweep = prop['sweep']
+    alphas = av.airfoil_data[:, 2]
+    Cl0 = av.airfoil_data[:, 3]
+    Cd0 = av.airfoil_data[:, 4]
+    Cl_valid = ~np.isnan(Cl0)
+    Cd_valid = ~np.isnan(Cd0)
+
+    Cl = np.interp(alpha * 180/np.pi, alphas[Cl_valid], Cl0[Cl_valid]) * np.cos(sweep) ** 2
+    Cd = np.interp(alpha * 180/np.pi, alphas[Cd_valid], Cd0[Cd_valid]) * np.cos(sweep) ** 2
+
+    prop['Cl_r'] = Cl
+    prop['Cd_r'] = Cd
 
     prop['dCl_dxc'] = 0.5 * np.ones((prop['nx'], prop['nr']))
     prop['dCd_dxc'] = 0.01 * np.ones((prop['nx'], prop['nr']))
@@ -305,8 +430,14 @@ if __name__ == "__main__":
     prop['dCl_dxc'] /= simps(prop['dCl_dxc'], xc, axis=0) # normalize by area under curve
     prop['dCd_dxc'] /= simps(prop['dCd_dxc'], xc, axis=0) # normalize by area under curve
 
-    radial_locus(oper, prop)
+    #radial_locus(oper, prop)
+    radial_locus_sweep(oper, prop)
+    operating_range(av)
     #chord_locus(oper, prop)
     #hanson_sweep(oper, prop)
 
     plt.show()
+
+
+if __name__ == "__main__":
+    main()

app/vis.py
@@ -31,7 +31,44 @@ class EventGLViewWidget(gl.GLViewWidget):
         if event.key() == Qt.Key.Key_Escape:
             self.escapePressed.emit()
         super().keyPressEvent(event)
-        
+
+
+def create_arrow(start, end, color=(1, 0, 0, 1), rfac=0.01):
+    # Create a line (shaft)
+    line_points = np.array([start, end])
+    line = gl.GLLinePlotItem(pos=line_points, color=color, width=2, antialias=True)
+
+    # Create an arrowhead (cone)
+    arrow_direction = np.array(end, dtype=float) - np.array(start, dtype=float)
+    arrow_length = np.linalg.norm(arrow_direction)
+
+    cylradius = rfac * arrow_length
+    coneradius = 2 * cylradius
+
+    z_axis = np.array([0, 0, 1])  # Default cone orientation along z-axis
+    rotation_vector = np.cross(z_axis, arrow_direction)
+    rotation_angle = np.arccos(np.dot(z_axis, arrow_direction)) * 180 / np.pi
+    
+    if np.linalg.norm(rotation_vector) > 0:
+        rotation_vector /= np.linalg.norm(rotation_vector)
+    else:
+        rotation_vector = z_axis
+
+    cylinder_meshdata = gl.MeshData.cylinder(rows=10, cols=20, radius=[cylradius, cylradius], length=arrow_length)
+    cylinder_mesh = gl.GLMeshItem(meshdata=cylinder_meshdata, smooth=True, color=color, shader='shaded')
+
+    # Create a cone mesh for the arrowhead
+    cone_meshdata = gl.MeshData.cylinder(rows=10, cols=20, radius=[coneradius, 0], length=coneradius*2)
+    cone_mesh = gl.GLMeshItem(meshdata=cone_meshdata, smooth=True, color=color, shader='shaded')
+
+    # Apply the rotation matrix to the cylinder and cone
+    cylinder_mesh.rotate(rotation_angle, *rotation_vector[0:3])
+    cone_mesh.rotate(rotation_angle, *rotation_vector[0:3])
+
+    cylinder_mesh.translate(*start)
+    cone_mesh.translate(*end)
+
+    return cylinder_mesh, cone_mesh
 
 class STLViewerWidget(QWidget):
     def __init__(self, parent=None, popup=False):
@@ -52,13 +89,19 @@ class STLViewerWidget(QWidget):
         self.edgeToggle.setChecked(False)
         self.edgeToggle.stateChanged.connect(self.update_mesh_plot)
 
+        self.arrowToggle = QCheckBox("Show Arrow")
+        self.arrowToggle.setChecked(False)
+        self.arrowToggle.stateChanged.connect(self.update_mesh_plot)
+
         self.save_button = QPushButton("Save to .stl")
         self.save_button.clicked.connect(self.save_stl_file)
 
         self.view.doubleClicked.connect(self.display_fullscreen_dialog)
 
         #viewSettingLayout.addWidget(self.smoothToggle, 0, 0)
+        viewSettingLayout.addWidget(self.arrowToggle, 0, 0)
         viewSettingLayout.addWidget(self.edgeToggle, 0, 1)
+        
 
         if not popup:
             viewSettingLayout.addWidget(self.save_button, 0, 2)
@@ -134,19 +177,22 @@ class STLViewerWidget(QWidget):
 
         intensities = np.zeros((self.num_blades, len(vertices) // 3))
 
-        
+        if self.arrowToggle.isChecked():
+            line, arrow = create_arrow([0, 0, 0], [0, 0, np.max(stl_mesh.v1)])
+            self.view.addItem(line)
+            self.view.addItem(arrow)
 
         # first compute intensities to get colour range
         for i in range(self.num_blades):
 
             theta = 2 * np.pi * i / self.num_blades
-            y_rot = np.array([
-                [np.cos(theta), 0, np.sin(theta)],
-                [0, 1, 0],
-                [-np.sin(theta), 0, np.cos(theta)]
+            z_rot = np.array([
+                [np.cos(theta), -np.sin(theta), 0],
+                [np.sin(theta), np.cos(theta), 0],
+                [0, 0, 1]
             ])
             
-            rotated_normals = np.dot(normals, y_rot)
+            rotated_normals = np.dot(normals, z_rot)
             intensity = np.dot(rotated_normals, light_dir)
             intensity_per_face = intensity[::3]
 
@@ -159,12 +205,12 @@ class STLViewerWidget(QWidget):
         for i in range(self.num_blades):
 
             theta = 2 * np.pi * i / self.num_blades
-            y_rot = np.array([
-                [np.cos(theta), 0, np.sin(theta)],
-                [0, 1, 0],
-                [-np.sin(theta), 0, np.cos(theta)]
+            z_rot = np.array([
+                [np.cos(theta), -np.sin(theta), 0],
+                [np.sin(theta), np.cos(theta), 0],
+                [0, 0, 1]
             ])
-            rotated_vertices = np.dot(vertices, y_rot)
+            rotated_vertices = np.dot(vertices, z_rot)
         
             intensity_normalized = (intensities[i] - min_intensity) / (
             max_intensity - min_intensity + 1e-8