12d324be83545349af332da37c23b7bd1a1a323f
lpender3672 <58957722+lpender3672@users.noreply.github.com>
2025-06-01 21:13:23 +0100

    will finish the draft tonight at all costs

--- app/results/torque_investigation.py
+++ app/results/torque_investigation.py
@@ -137,7 +137,7 @@ def compare_torque(prop_result_path):
 if __name__ == "__main__":
 
     fig, ax = compare_torque(
-        'app/results/foxeer_toroidal.prop'
+        'app/results/dalprop5045bnr.prop'
     )
 
     fig.savefig(
